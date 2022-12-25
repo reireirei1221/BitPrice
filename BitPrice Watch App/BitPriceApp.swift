7997d73dbcabacb80c0da86f500c97670ec4e0e1
reireirei1221 <reireirei1221@icloud.com>
2022-12-25 12:53:02 +0900

    Initial Commit

---
+++ BitPrice Watch App/BitPriceApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitPriceApp.swift
+//  BitPrice Watch App
+//
+//  Created by 澤野令 on 2022/12/25.
+//
+
+import SwiftUI
+
+@main
+struct BitPrice_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
